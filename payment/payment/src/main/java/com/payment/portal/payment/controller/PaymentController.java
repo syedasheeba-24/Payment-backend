b7463d7dad8b4b901dd1cabcf361bededddcd818
Asif,Syeda Sheeba <Syeda.Asif@cerner.com>
2022-11-20 14:56:21 +0530

    adding cors

--- payment/payment/src/main/java/com/payment/portal/payment/controller/PaymentController.java
+++ payment/payment/src/main/java/com/payment/portal/payment/controller/PaymentController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,6 +61,7 @@ public class PaymentController {
 		beneficiaryRepository.save(beneficiary);
 	}
 	
+	@CrossOrigin
 	@PutMapping("/updateOwner")
 	public void updateOwner(@RequestBody Owner owner) {
 		ownerRepository.save(owner);
